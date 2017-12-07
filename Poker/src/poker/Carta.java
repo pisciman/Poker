5e5231209e129d63b909e0f8cb2e4e53cae86378
Alessio Torricelli <Alessio92845@gmail.com>
2017-12-07 20:48:44 +0100

    Carta Finita

--- Poker/src/poker/Carta.java
+++ Poker/src/poker/Carta.java
@@ -1,6 +1,20 @@
-
+/**
+ * @author Alessio Torricelli
+ * @version 1.0
+ * @since 07-12-2017
+**/
 package poker;
 
-public class Carta {
-    
+public class Carta
+{
+    private String simbolo;
+    private int valore;
+    private boolean stato;
+
+    public Carta(String simbolo, int valore, boolean stato)
+    {
+        this.simbolo = simbolo;
+        this.valore = valore;
+        this.stato = stato;
+    }
 }
