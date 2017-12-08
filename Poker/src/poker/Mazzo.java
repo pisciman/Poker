997b1b52fed27c3533399832412ebb3db8f95951
Alessio Torricelli <Alessio92845@gmail.com>
2017-12-08 10:45:59 +0100

    Finito Classe Carta e Mazzo

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -10,10 +10,13 @@ public class Mazzo
     private final int MAX1 = 52;
     private final int MAX2 = 13;
     private Carta[] mazzo;
-
-    public Mazzo(Carta[] mazzo)
+    
+    public static void main(String[] args)
     {
-        this.mazzo = mazzo;
+        Mazzo m = new Mazzo();
+        System.out.println(m.toString());
+        m.creaMazzo();
+        System.out.println(m.toString());
     }
     
     public void creaMazzo()
@@ -21,11 +24,33 @@ public class Mazzo
         String[] simboli = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         
         /* Indice del Mazzo [0], Indice dei Simboli [1], Indice del Seme [2] */
-        for(int[] i = {0, 0, 0}; i[0] < MAX1; i[0]++)
+        for(int[] i = {0, 0, 0}; i[0] < MAX1; i[0]++, i[1]++)
         {
             mazzo[i[0]] = new Carta(simboli[i[1]], i[2], false);
             
-            if(i[1] == MAX2) { i[1] = 0; i[2]++; }
+            //if(i[1] < MAX2 - 1) { i[1] = 0; i[2]++; }
         }
+    }
+    
+    public void mescolaMazzo()
+    {
+        Carta scambio;
+        int[] pos = {0, 0};
+        
+        for(int i = 0; i < 100; i++)
+        {
+            pos[0] = (int) (Math.random() * MAX1);
+            pos[1] = (int) (Math.random() * MAX1);
+            
+            scambio = mazzo[pos[0]];
+            mazzo[pos[0]] = mazzo[pos[1]];
+            mazzo[pos[1]] = scambio;
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Mazzo{" + "mazzo=" + mazzo + '}';
     }
 }
