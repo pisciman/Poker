fae91f01afd1cddbd5f3715a0491baefe24d3753
Simone Nicolini <32368684+simplayy@users.noreply.github.com>
2017-12-10 20:20:53 +0100

    mazzo incompleto

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -1,6 +1,15 @@
 
-package poker;
-
+package poker
 public class Mazzo {
-    
+private final Carta[] mazzo = new Carta[52];
+
+public Mazzo() {
+int c = 0;
+    for (int j=0; j <=4; j++) {
+        for (int i=0; i <=13; i++) { 
+            mazzo[c] = new Carta(j,i,c);
+            c++;
+        }
+    }
+}
 }
