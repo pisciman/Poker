e43ea2db8ea863d90927e5f1e46a8ceebecd3126
MrcSossy <mrcsossy@gmail.com>
2017-12-11 12:12:43 +0100

    Prima implementazione della Carta

--- Poker/src/poker/Carta.java
+++ Poker/src/poker/Carta.java
@@ -1,6 +1,27 @@
-
 package poker;
 
+/**
+ * Definisce una carta da gioco del poker classica
+ * costituita dal <code>seme</code>, dal <code>valore</code> e
+ * dallo stato <code>coperto</code> di ogni carta.
+ *
+ * @author m.santosuosso
+ */
 public class Carta {
+    /**Seme della carta*/
+    final private String seme;
+    /**Valore (numero) della carta*/
+    final private int valore;
+    /**Definisce lo stato (coperto(1) o scoperto(0) della carta)*/
+    private boolean coperto;
+
+/*--Variabili-----------------------------------------------------------------*/
     
+    public Carta(String seme, int valore, boolean coperto) {
+        this.seme = seme;
+        this.valore = valore;
+        this.coperto = coperto;
+    }
+    
+/*--Costruttore---------------------------------------------------------------*/
 }
