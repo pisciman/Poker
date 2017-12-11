730ac1f0773a91a9fac119521a93de43644a12bc
MrcSossy <mrcsossy@gmail.com>
2017-12-11 12:27:29 +0100

    Prima implementazione del mazzo

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -1,6 +1,24 @@
-
 package poker;
 
+/**
+ * 
+ *
+ * @author m.santosuosso
+ */
 public class Mazzo {
+    private Carta mazzo[] = new Carta[52];
+    String semi [] = {"Cuori", "Quadri", "Fiori", "Picche"};
     
+/*--Variabili-----------------------------------------------------------------*/
+
+    /**
+     * Genera in modo ordinato un mazzo di 52 carte da poker coperte
+     */
+    public void generate() {
+       for (int tempSeme = 0; tempSeme < 4; tempSeme++)
+            for (int tempValore = 0; tempValore < 13; tempValore++)
+                mazzo[tempValore + (13 * tempSeme)] = new Carta(semi[tempSeme], tempValore, true);
+    }
+    
+/*--Metodo generate-----------------------------------------------------------*/
 }
