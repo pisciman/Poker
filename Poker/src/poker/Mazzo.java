81c39087b748da81679514df7caa8bec33e55ffc
d.santacroce <d.santacroce@Lab-Info-16.VINCI.IISCORNI.IT>
2017-12-15 09:43:32 +0100

    fatto funzione creaMazzo

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -2,5 +2,17 @@
 package poker;
 
 public class Mazzo {
-    
+    Carta[] c;
+    int q=0;
+   
+   public Carta[] creaMazzo(){
+       for(int i=0; i<4; i++) {
+           for(int y=1;y<14;y++){
+               c[q]=new Carta(y,i);
+           }
+    }
+       return c;
+   }
+        
+
 }
