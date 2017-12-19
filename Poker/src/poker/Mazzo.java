0e417058179134cf542e06497922edb8f7c79346
Simone Nicolini <32368684+simplayy@users.noreply.github.com>
2017-12-19 17:10:10 +0100

    mazzo finito

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -1,24 +1,44 @@
-
 package poker;
 import java.util.*;
 public class Mazzo {
 private final Carta[] mazzo = new Carta[52];
 private int icarte=52;
 
+/**
+ * Genera il mazzo creando 52 carte
+ */
 public Mazzo() {
+    
 int c = 0;
-    for (int j=0; j <=4; j++) {
-        for (int i=0; i <=13; i++) { 
-            mazzo[c] = new Carta(j,i,c);
-            c++;
+    for (int j=1; j <=4; j++) {
+        for (int i=1; i <=13; i++) { 
+            mazzo[c] = new Carta(j,i);
+            c++;            
         }
     }
+    for (int i = 0; i < 52; i++) {
+        System.out.println(mazzo[i]);
+    }
+    
+ /*   System.out.println();         prova funzione mescola
+    
+    Mescola();  Mescola();  Mescola();  Mescola();
+      
+        for (int i = 0; i < 52; i++) {
+        System.out.println(mazzo[i]);
+    }   */   
+ 
 }
-
+/**
+ * Funzione che mescola la carta
+ */
 private void Mescola(){
     Collections.shuffle(Arrays.asList(mazzo));
 }
 
+/**
+ * Pesca una carta
+ */
 public Carta pesca(){
     icarte--;
     return mazzo[icarte];
