a305f979a7e2203e73b826373494edd3ab3f79bf
Marcello Missiroli <prof.missiroli@gmail.com>
2018-01-09 12:34:40 +0100

    Giocatore: Spostato Main. TODO: toString(), Tests.

--- Poker/src/poker/Giocatore.java
+++ Poker/src/poker/Giocatore.java
@@ -2,8 +2,10 @@ package poker;
 
 //import java.util.Scanner;
 
+import java.util.Scanner;
+
 /**
- * @author Torricelli Alessio
+ * @author Torricelli Alessio Alessio9284
  * @author Pollarini Umberto
  * @author Sferruzza Matteo
  */
@@ -57,5 +59,41 @@ public class Giocatore
 
     public void setSoldi(int soldi) {
         this.soldi = soldi;
+    }
+
+    public String toString() {
+        //TODO: Da fare
+       return "";
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        String nome;
+        char sesso;
+        int età, soldi;
+
+        //I parametri dell'oggetto giocatore sono per semplicità
+        Giocatore g = new Giocatore("Umberto", 'M', 18, 1000);
+
+        //dovendo fare "l'iscrizione dell'utente" molto probabilmente useremo questo codice
+
+        System.out.print("Inserisci il tuo nome: ");
+        nome = input.nextLine();
+        g.setNomeGiocatore(nome);
+
+        System.out.print("Inserire il sesso ([ M ] Maschio, [ F ] Femmina): ");
+        sesso = input.nextLine().charAt(0);
+        g.setSesso(sesso);
+
+        System.out.print("Inserisci la tua età: ");
+        età = input.nextInt();
+        g.setEtà(età);
+
+        System.out.print("Inserisci il tuo budget totale");
+        soldi = input.nextInt();
+        g.setSoldi(soldi);
+
     }
 }
--- Poker/src/poker/Poker.java
+++ Poker/src/poker/Poker.java
@@ -1,40 +1,8 @@
-/**
- * @author Torricelli Alessio
- * @author Pollarini Umberto
- */
+
 package poker;
 
 import java.util.Scanner;
 
 public class Poker {
-    public static void main(String[] args) 
-    {
-        Scanner input = new Scanner(System.in);
-        
-        String nome;
-        char sesso;
-        int età, soldi;
-        
-        //I parametri dell'oggetto giocatore sono per semplicità
-        Giocatore g = new Giocatore("Umberto", 'M', 18, 1000);
-        
-        //dovendo fare "l'iscrizione dell'utente" molto probabilmente useremo questo codice
-        /*
-        System.out.print("Inserisci il tuo nome: ");
-        nome = input.nextLine();
-        g.setNomeGiocatore(nome);
-        
-        System.out.print("Inserire il sesso ([ M ] Maschio, [ F ] Femmina): ");
-        sesso = input.nextLine().charAt(0);
-        g.setSesso(sesso);
-        
-        System.out.print("Inserisci la tua età: ");
-        età = input.nextInt();
-        g.setEtà(età);
-        
-        System.out.print("Inserisci il tuo budget totale");
-        soldi = input.nextInt();
-        g.setSoldi(soldi);
-        */
-    }
+
 }
