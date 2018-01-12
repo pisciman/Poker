ec44ee1994f1cc45bd56752c2a6daabd201f40cd
Alessio Torricelli <Alessio92845@gmail.com>
2018-01-12 16:01:27 +0100

    Mazzo con Costruttore (Funzionante!)

--- Poker/src/poker/Mazzo.java
+++ Poker/src/poker/Mazzo.java
@@ -7,20 +7,28 @@ import java.util.Collections;
 * Regola il mazzo di carte con cui giocare
 *
 * @author m.santosuosso
-* @author a.torricelli
+* @author a.torricelli (Costruttore e toString)
 * @author m.missiroli (main)
 * @author s.nicolini
 * @author m.missiroli (adattamento genera(), da testare)
 */
 public class Mazzo {
-    /**Mazzo da gioco*/
+    /* Mazzo da gioco */
     private final int MAX = 52;
     private Carta mazzo[] = new Carta[MAX];
     private int icarte = MAX;
 
     /**
-    * Genera in modo ordinato il mazzo di 52 carte da poker coperte
-    */
+     * Costruttore del Mazzo
+     */
+    public Mazzo() {
+        genera();
+        mescola();
+    }
+    
+    /**
+     * Genera in modo ordinato il mazzo di 52 carte da poker coperte
+     */
     public void genera() {
         for (Seme seme : Seme.values())
         for (Valore valore: Valore.values())
@@ -28,37 +36,37 @@ public class Mazzo {
     }
 
     /**
-    * Genera in modo ordinato il mazzo di 52 carte da poker coperte
-    * TODO: Migliorare l'aspetto
-    */
+     * Stampa 52 carte da poker coperte
+     * TODO: Migliorare l'aspetto
+     * @return (<code>String</code>)
+     */
     @Override
     public String toString() {
-        String str = "";
+        String str = "Mazzo {";
 
         for(Carta c : mazzo)
-        str += "\n    " + c;
-
-        return "Mazzo\n{" + str + "\n}";
+            str += "\n    " + c.toString();
+        
+        return str + "\n}";
     }
 
     /**
-    * Funzione che mescola la carta
-    */
+     * Funzione che mescola la carta
+     */
     private void mescola() {
         Collections.shuffle(Arrays.asList(mazzo));
     }
 
     /**
-    * Pesca una carta
-    */
+     * Pesca una carta
+     */
     public Carta pesca(){
         icarte--;
         return mazzo[icarte];
     }
 
-    // DA COME SI VEDE, QUESTO ANCORA NON FUNZIONA.
     public static void main (String args[]) {
         Mazzo m = new Mazzo();
-        System.out.println(m);
+        System.out.println(m.toString());
     }
 }
