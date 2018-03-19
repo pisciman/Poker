9ded3aafd1594cd05c039e896b14d6cf42c9dc37
Francesco Metta <francescometta@MacBook-Pro-di-Francesco.local>
2018-03-19 09:47:08 +0100

    Abati & Metta: Gui

---
+++ Poker/src/gui/Board.java
@@ -0,0 +1,41 @@
+package gui;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author francescometta
+ */
+public class Board extends JFrame
+{
+    // ATTRIBUTES
+    private final Dashboard dash;
+    
+    public Board(String title, int width, int height) {
+        super(title); 
+        this.dash = new Dashboard(width, height, new BorderLayout());
+        craft();
+    }
+    
+    private void craft() {
+        add(dash);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setUndecorated(true);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+    
+    public static void main(String[] args) {
+        new Board("Poker", 1280, 720);
+    }
+}
