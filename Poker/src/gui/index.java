dde43989464e8379ba1ba104e60af2eb158d12ed
Simone Nicolini <32368684+simplayy@users.noreply.github.com>
2018-04-04 12:51:04 +0200

    creo main della pagina iniziale

---
+++ Poker/src/gui/index.java
@@ -0,0 +1,18 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poker;
+
+/**
+ *
+ * @author s.nicolini
+ */
+public class index {
+    public static void main(String[] args) {
+
+        frameindex home=new frameindex();
+    }
+}
