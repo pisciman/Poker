418fd5fae7db4f8626a59ee230ffed2be2897c38
Marcello Missiroli <prof.missiroli@gmail.com>
2018-05-31 08:19:40 +0200

    DashboardTest fix, elements eliminato

--- Poker/test/gui/DashboardTest.java
+++ Poker/test/gui/DashboardTest.java
@@ -1,6 +1,6 @@
 package gui;
 
-import elements.Player;
+import poker.Giocatore;
 import org.junit.Test;
 
 import java.awt.*;
@@ -9,9 +9,9 @@ import static org.junit.Assert.*;
 
 public class DashboardTest {
     @Test
-    public void loadImageTest( ) throws Exception {
+    public void loadBasicTest( ) throws Exception {
 
-        Player p1 = new Player("NOME", 'M', 18, 300);
+        Giocatore p1 = new Giocatore("NOME", 'M', 18, 300);
         Dashboard dashboard = new Dashboard(new GridBagLayout(),p1);
         assertTrue(dashboard.table != null);
 
